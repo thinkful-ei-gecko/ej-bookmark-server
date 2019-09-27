40cd401807f09bf93d74d1cf719adac0099a81c6
EJ <ejg132@gmail.com>
2019-09-27 10:26:37 -0700

    finished product

---
+++ app.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const uuid = require('uuid/v4');
+const { NODE_ENV } = require('./config');
+const bookmarkRouter = require('./Bookmark.js');
+
+const app = express();
+
+//security for the application
+
+const morganOption = (NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common';
+
+app.use(morgan(morganOption))
+app.use(helmet())
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  // move to the next middleware
+  next()
+})
+
+app.use(bookmarkRouter);
+
+//error handling (default)
+
+app.use(function errorHandler(error, req, res, next) {
+       let response
+       if (NODE_ENV === 'production') {
+         response = { error: { message: 'server error' } }
+       } else {
+         console.error(error)
+         response = { message: error.message, error }
+       }
+       res.status(500).json(response)
+     })
+
+app.use(cors())
+
+module.exports = app
---
+++ store.js
@@ -0,0 +1,11 @@
+const bookMarks = [
+    {
+      title: "Harry Potter",
+      url: "https://openlibrary.org/works/OL262598W/The_Memoirs_of_Sherlock_Holmes",
+      desc: "Boring plot",
+      rating: 4,
+      id: [1]
+    }
+  ]
+
+module.exports = {bookMarks}
